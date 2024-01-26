34d9bd5909d3f3c591cb7d859249e0147148e5e9
AbanobNageh <abanob.nageh159@gmail.com>
2024-01-26 09:20:10 +0200

    Refactored the deep-linking code.

--- app/src/main/java/com/abanobnageh/recipeapp/MainActivity.kt
+++ app/src/main/java/com/abanobnageh/recipeapp/MainActivity.kt
@@ -1,7 +1,5 @@
 package com.abanobnageh.recipeapp
 
-import android.content.BroadcastReceiver
-import android.content.Context
 import android.content.Intent
 import android.net.Uri
 import android.os.Bundle
@@ -14,14 +12,13 @@ import cafe.adriel.voyager.core.screen.Screen
 import cafe.adriel.voyager.navigator.Navigator
 import com.abanobnageh.recipeapp.core.theme.RecipeAppTheme
 import com.abanobnageh.recipeapp.core.viewmodel.MainActivityViewModel
-import com.abanobnageh.recipeapp.feature_recipes.screens.RecipeListScreen
-import com.abanobnageh.recipeapp.feature_recipes.screens.RecipeScreen
+import com.abanobnageh.recipeapp.utils.getScreenListFromDeepLink
 import dagger.hilt.android.AndroidEntryPoint
 
 @OptIn(ExperimentalMaterialApi::class)
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
-    val activityViewModel: MainActivityViewModel by viewModels()
+    private val activityViewModel: MainActivityViewModel by viewModels()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -64,40 +61,4 @@ fun App(
             screenList
         )
     }
-}
-
-@ExperimentalMaterialApi
-fun getScreenListFromDeepLink(deepLinkData: Uri?): List<Screen> {
-    val pathSegments = deepLinkData?.pathSegments
-
-    if (pathSegments.isNullOrEmpty()) {
-        return listOf(RecipeListScreen())
-    }
-    else {
-        val screenList = mutableListOf<Screen>()
-        val primarySegment = pathSegments[0]
-
-        when (primarySegment) {
-            PrimaryPathSegment.RECIPES.segmentValue -> {
-                screenList.add(RecipeListScreen())
-
-                if (pathSegments.size == 2) {
-                    val recipeId = pathSegments[1].toIntOrNull()
-
-                    recipeId?.let {
-                        screenList.add(RecipeScreen(it))
-                    }
-                }
-            }
-            else -> {
-                screenList.add(RecipeListScreen())
-            }
-        }
-
-        return screenList
-    }
-}
-
-enum class PrimaryPathSegment(val segmentValue: String) {
-    RECIPES("recipes")
 }
